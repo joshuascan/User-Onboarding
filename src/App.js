81933cf19c306f42485c5585f29ec664a412a54e
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-19 13:10:03 -0700

    Set initial states

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 import Form from './components/Form'
+import axios from 'axios'
+import * as yup from 'yup'
+
+const initialFormValues = {
+    name: '',
+    email: '',
+    password: '',
+    termsOfService: false,
+}
+
+const initialFormErrors = {
+    name: '',
+    email: '',
+    password: '',
+}
+
+const initialUsers = []
+const initialDisabled = true
+
 
 function App() {
+
+    const [users, setUsers] = useState(initialUsers)
+    const [formValues, setFormValues] = useState(initialFormValues)
+    const [formErrors, setFormErrors] = useState(initialFormErrors)
+    const [disabled, setDisabled] = useState(initialDisabled)
+
   return (
     <div className="App">
         {/* <Form /> */}
