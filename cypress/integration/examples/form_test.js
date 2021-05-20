426a8f21046694337d74d0b6a9dfca5ccdcf0e0e
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-20 13:40:55 -0700

    Add test for name input

--- cypress/integration/examples/form_test.js
+++ cypress/integration/examples/form_test.js
@@ -1,5 +1,25 @@
 describe('User Sign-up App', () => {
     beforeEach(() => {
-        cy.visit('http://localhost:3000/');
-    });
+        cy.visit('http://localhost:3000/')
+    })
+
+    const nameInput = () => cy.get('input[name=name]')
+    const emailInput = () => cy.get('input[name=email]')
+    const passwordInput = () => cy.get('input[name=password]')
+
+    it('the proper elements are showing', () => {
+        nameInput().should('exist')
+        emailInput().should('exist')
+        passwordInput().should('exist')
+    })
+    
+    describe('Filling out the inputs', () => {
+        it('can type in the inputs', () => {
+            nameInput()
+                .should('have.value', '')
+                .type('Josh Scanlan')
+                .should('have.value', 'Josh Scanlan')
+        })
+    })
+
 })
